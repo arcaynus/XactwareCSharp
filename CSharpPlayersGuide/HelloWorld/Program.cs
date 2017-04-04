8e077fac516a198905e4c5d022ef0d9e4d9c879b
Andre Morales <supyrman2002@yahoo.com>
2017-04-04 16:12:15 -0600

    Added Creating variables project

--- CSharpPlayersGuide/HelloWorld/Program.cs
+++ CSharpPlayersGuide/HelloWorld/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-
+// NameSpace is largest grouping in C#
 namespace HelloWorld
 {
+    // Class Program cointaining the Main Method
     class Program
     {
+
+        /* Main Entry point of the program */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!"); //Removed because book wanted something different
+            Console.WriteLine("I'm Sorry, Dave. I'm afraid I can't do that."); //From the movie 2001 Space Odessey
+            Console.ReadKey(); //Readkey to prevent console window from closing 
         }
     }
 }
