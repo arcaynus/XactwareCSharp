950f8c8df25d1a63a86a83403b4cd95cfc0ad2d0
Andre Morales <supyrman2002@yahoo.com>
2017-04-06 08:11:33 -0600

    Adding Variables Project

--- CSharpPlayersGuide/Chapter_5_Creating_Variables/Program.cs
+++ CSharpPlayersGuide/Chapter_5_Creating_Variables/Program.cs
@@ -10,7 +10,23 @@ namespace Chapter_5_Creating_Variables
     {
         static void Main(string[] args)
         {
-            int score;
+            int player_score; // declaring a variable
+            player_score = 0; // initializing a variable
+            int score = 3; // declaring and assigning a variable in one line
+
+            Console.WriteLine(score); // Accessing the value of a variable to print;
+
+            int a = 5;
+            int b = 2;
+            b = a; // Assign a copy of the value of "a" to variable "b"
+            a = -3;
+            Console.WriteLine($"a: {a} b:{b}");
+
+            // Creating multiple variables on one line
+            int A, B, C;
+            A = B = C = 10;
+
+            Console.ReadKey();
         }
     }
 }
