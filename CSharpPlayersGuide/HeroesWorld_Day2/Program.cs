30cc707deea83dac48b58e652de1e3304ca8bd18
Andre Morales <supyrman2002@yahoo.com>
2017-04-23 22:22:48 -0600

    Adding assignment for HeroesWorld Day two

--- CSharpPlayersGuide/HeroesWorld_Day2/Program.cs
+++ CSharpPlayersGuide/HeroesWorld_Day2/Program.cs
@@ -9,7 +9,8 @@ namespace HeroesWorld_Day2
     class Program
     {
         static private string[] invalidNames = new[] { "Legolas", "Gandalf", "Frodo", "Bilbo" };
-        enum Statistics
+
+        private enum Statistics
         {
             strength = 0,
             perception,
@@ -17,91 +18,36 @@ namespace HeroesWorld_Day2
             charisma,
             intelligence,
             agility,
-            luck
+            luck,
+            hitpoints
         };
+        public enum Dice { d4 = 5, d6 = 7, d8 = 9, d10 = 11, d00 = 11, d20 = 21 };
 
         static void Main(string[] args)
         {
 
             string name = CheckForInvalidNames();
             int[] statistics = new int[Enum.GetNames(typeof(Statistics)).Length];
-            
+
             // Replace this with Generate Stats Method - Start //
-            // Statistics
-            int strength = 0;
-            int perception = 0;
-            int endurance = 0;
-            int charisma = 0;
-            int intelligence = 0;
-            int agility = 0;
-            int luck = 0;
-
-            // Derived Stats
-            int hitpoints = 0;
-
-
-            int totalValueMinimum = 35;
-            int playerTotalStats = 0;
-            int rollCount = 1;
-
-
-            Random random = new Random();
-            //int randomNumber = random.Next(0, 100); // This will return integers between 0-99;
-
             
+            Random random = new Random();
+
+
             Console.WriteLine("Rolling your awesome stats...");
             // Generate Stats:
-            while (playerTotalStats <= totalValueMinimum)
-            {
-                //int strength;
-                //int perception;
-                //int endurance;
-                //int charisma;
-                //int intelligence;
-                //int agility;
-                //int luck;
-                strength = random.Next(1, 11);
-                perception = random.Next(1, 11);
-                endurance = random.Next(1, 11);
-                charisma = random.Next(1, 11);
-                intelligence = random.Next(1, 11);
-                agility = random.Next(1, 11);
-                luck = random.Next(1, 11);
-
-                // Generate derived stats
-                hitpoints = 10 + endurance;
-
-
-                playerTotalStats = strength + perception + endurance + charisma + intelligence + agility + luck;
-                Console.WriteLine($"Roll: {rollCount++} We Rolled a total of: {playerTotalStats}");
-
-            }
+            GenerateStats(random, statistics);
 
             // Replace this with Generate Stats Method - End //
 
 
             Console.WriteLine($"Welcome to HerosWorld (tm): {name}");
-            
+
             // Create a Display Stats Function - Start //
 
             // Display Stats
 
-
-
-            Console.WriteLine("Your Amazing Base Stats");
-            Console.WriteLine("________________________");
-            Console.WriteLine($"Strength: {strength}");
-            Console.WriteLine($"Perception: {perception}");
-            Console.WriteLine($"Endurance: {endurance}");
-            Console.WriteLine($"Charisma: {charisma}");
-            Console.WriteLine($"Intelligence: {intelligence}");
-            Console.WriteLine($"Agility: {agility}");
-            Console.WriteLine($"Luck: {luck}");
-            Console.WriteLine("________________________");
-
-            Console.WriteLine("Your Amazing Derived Stats");
-            Console.WriteLine("________________________");
-            Console.WriteLine($"Hit Points: {hitpoints}");
+            PrintStatistics(statistics);
 
             // Create a Display Stats Function - End //
 
@@ -109,9 +55,9 @@ namespace HeroesWorld_Day2
             Console.WriteLine("You encounter an rabid emotionally distant exploding feral deseased lonely slightly overweight kitten");
 
             int kittenHP = 10;
-            while (hitpoints > 0 || kittenHP > 0)
+            while (statistics[(int)Statistics.hitpoints] > 0 || kittenHP > 0)
             {
-                Console.WriteLine($"Player HP: {hitpoints}\nKitten HP: {kittenHP}");
+                Console.WriteLine($"Player HP: {statistics[(int)Statistics.hitpoints]}\nKitten HP: {kittenHP}");
                 // Roll for initiative
                 var yourRoll = random.Next(1, 21);
                 var kittenRoll = random.Next(1, 21);
@@ -127,9 +73,9 @@ namespace HeroesWorld_Day2
                     }
                     //Kitten attacks first
                     var kittenDamage = random.Next(0, 3);
-                    hitpoints -= kittenDamage;
+                    statistics[(int)Statistics.hitpoints] -= kittenDamage;
                     Console.WriteLine($"The kitten purred at you and did: {kittenDamage}");
-                    if (hitpoints <= 0)
+                    if (statistics[(int)Statistics.hitpoints] <= 0)
                     {
                         break;
                     }
@@ -138,9 +84,9 @@ namespace HeroesWorld_Day2
                 {
                     //Kitten attacks first
                     var kittenDamage = random.Next(0, 3);
-                    hitpoints -= kittenDamage;
+                    statistics[(int)Statistics.hitpoints] -= kittenDamage;
                     Console.WriteLine($"The kitten purred at you and did: {kittenDamage}");
-                    if (hitpoints <= 0)
+                    if (statistics[(int)Statistics.hitpoints] <= 0)
                     {
                         break;
                     }
@@ -155,7 +101,7 @@ namespace HeroesWorld_Day2
                 Console.ReadKey();
 
             }
-            if (hitpoints < kittenHP)
+            if (statistics[(int)Statistics.hitpoints] < kittenHP)
             {
                 Console.WriteLine("Player Lost");
             }
@@ -169,7 +115,7 @@ namespace HeroesWorld_Day2
         static string CheckForInvalidNames()
         {
             Console.WriteLine("Please Enter a Character's Name");
-            string name="";
+            string name = "";
 
             bool validName = false;
             while (!validName)
@@ -200,10 +146,63 @@ namespace HeroesWorld_Day2
             return name;
         }
 
-        static int[] GenerateStats(int [] stats)
+        static int[] GenerateStats(Random random, int[] stats)
         {
+            int totalValueMinimum = 35;
+            int playerTotalStats = 0;
+            int rollCount = 1;
 
+            while (playerTotalStats <= totalValueMinimum)
+            {
+                //int strength;
+                //int perception;
+                //int endurance;
+                //int charisma;
+                //int intelligence;
+                //int agility;
+                //int luck;
+                stats[(int)Statistics.strength] = random.Next(1, 11);
+                stats[(int)Statistics.perception] = random.Next(1, 11);
+                stats[(int)Statistics.endurance] = random.Next(1, 11);
+                stats[(int)Statistics.charisma] = random.Next(1, 11);
+                stats[(int)Statistics.intelligence] = random.Next(1, 11);
+                stats[(int)Statistics.agility] = random.Next(1, 11);
+                stats[(int)Statistics.luck] = random.Next(1, 11);
+
+                // Generate derived stats
+                stats[(int)Statistics.hitpoints] = 10 + stats[(int)Statistics.endurance];
+
+
+                playerTotalStats = GetTotalStats(stats);
+                Console.WriteLine($"Roll: {rollCount++} We Rolled a total of: {playerTotalStats}");
+
+            }
             return null;
+        }
+        public static int GetTotalStats(int[] stats)
+        {
+            return stats[(int)Statistics.strength] + stats[(int)Statistics.perception] + stats[(int)Statistics.endurance] + stats[(int)Statistics.charisma] + stats[(int)Statistics.intelligence] + stats[(int)Statistics.agility] + stats[(int)Statistics.luck];
+        }
+        public static int RollDice(Random rand, Dice type)
+        {
+            return rand.Next(0, (int)type);
+        }
+        public static void PrintStatistics(int[] stats)
+        {
+            Console.WriteLine("Your Amazing Base Stats");
+            Console.WriteLine("________________________");
+            Console.WriteLine($"Strength: {stats[(int)Statistics.strength]}");
+            Console.WriteLine($"Perception: {stats[(int)Statistics.perception]}");
+            Console.WriteLine($"Endurance: {stats[(int)Statistics.endurance]}");
+            Console.WriteLine($"Charisma: {stats[(int)Statistics.charisma]}");
+            Console.WriteLine($"Intelligence: {stats[(int)Statistics.intelligence]}");
+            Console.WriteLine($"Agility: {stats[(int)Statistics.agility]}");
+            Console.WriteLine($"Luck: {stats[(int)Statistics.luck]}");
+            Console.WriteLine("________________________");
+
+            Console.WriteLine("Your Amazing Derived Stats");
+            Console.WriteLine("________________________");
+            Console.WriteLine($"Hit Points: {stats[(int)Statistics.hitpoints]}");
         }
     }
 }
